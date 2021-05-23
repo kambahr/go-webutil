cfefe94b75e074805d18ac3877430bf797ceaa18
Kamiar Bahri <kbahri@hotmail.com>
2021-05-23 17:51:44 +0100

    added RemoveCommentsFromByBiteArry() and RemoveCommentsFromString(). They can be used to remove blocks of (internal) comments that will not be visible while the website is running.

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package webutil
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
@@ -72,4 +73,74 @@ func getFileExtension(p string) string {
 	}
 
 	return extx
+}
+
+// RemoveCommentsFromByBiteArry removes a block of text from a byte array.
+func (h *HTTP) RemoveCommentsFromByBiteArry(b []byte, begin string, end string) []byte {
+
+	// Avoid recursion by using a label to go through
+	// many iterations until all target blocks of text are removed.
+lblAgain:
+	i := bytes.Index(b, []byte(begin))
+
+	if i < 0 {
+		// not found
+		return b
+	}
+
+	left := b[:i-1]
+	right := b[len(left):]
+
+	j := bytes.Index(right, []byte(end))
+	right = right[j+2:]
+
+	b = make([]byte, len(left)+len(right))
+
+	k := 0
+	for k = 0; k < len(left); k++ {
+		b[k] = left[k]
+	}
+
+	for p := 0; p < len(right); p++ {
+		b[k] = right[p]
+		k++
+	}
+
+	i = bytes.Index(b, []byte(begin))
+
+	if i > -1 {
+		goto lblAgain
+	}
+
+	return b
+}
+
+//RemoveCommentsFromFromString removes a block of text from inside an string.
+func (h *HTTP) RemoveCommentsFromFromString(s string, begin string, end string) string {
+
+	// Avoid recursion by using a label to go through
+	// many iterations until all target blocks of text are removed.
+lblAgain:
+	i := strings.Index(s, begin)
+
+	if i < 0 {
+		// not found
+		return s
+	}
+
+	left := s[:i-1]
+	right := s[len(left):]
+
+	j := strings.Index(right, end)
+	right = right[j+2:]
+
+	s = fmt.Sprintf("%s%s", left, right)
+
+	i = strings.Index(s, begin)
+
+	if i > -1 {
+		goto lblAgain
+	}
+
+	return s
 }
